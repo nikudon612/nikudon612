be094f8f70d9dc40b4d8c60afacbcb7b16dc7f44
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-02-25 14:59:22 -0500

    Added about js and css in pages folder

---
+++ src/pages/about/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about-content">
+      <p>
+        Full-Stack Software Developer with a background in Photography. My 10
+        years as a Photographer...
+      </p>
+    </div>
+  );
+}
+
+export default About;
