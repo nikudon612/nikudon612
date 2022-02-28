7e3a7970d819599bb0f962401d89fd010c259515
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-02-28 18:50:52 -0500

    Changed home layout and flexbox + made image perfectly square with object position css

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,33 +6,36 @@ function Home() {
   return (
     <div className="home">
       <div className="home-title">
-        <h1 className="info">CODE</h1>
-        <h1 className="info">& PHOTO</h1>
+        <div className="home-info">
+          <h1 className="info">CODE</h1>
+          <h1 className="info">& PHOTO</h1>
 
-        <h4 className="title">Full-Stack Software Developer</h4>
-        <h4 className="title" id="photographer">
-          Photographer
-        </h4>
-
-        <h2>
-          Hi, I'm Nick Bechtel,
-          <br />a{" "}
-          <Link id="sd" to="about" smooth={true} duration="1000">
-            Software Developer
-          </Link>
-          <br />
-          and{" "}
-          <a
-            href="https://www.nickbechtel.com/"
-            target="_blank"
-            rel="nooponer noreferrer"
-          >
+          <h4 className="title">Full-Stack Software Developer</h4>
+          <h4 className="title" id="photographer">
             Photographer
-          </a>{" "}
-          from NYC.
-        </h2>
+          </h4>
+
+          <h2 id="intro">
+            Hi, I'm Nick Bechtel,
+            <br />a{" "}
+            <Link id="sd" to="about" smooth={true} duration="1000">
+              Software Developer
+            </Link>
+            <br />
+            and{" "}
+            <a
+              href="https://www.nickbechtel.com/"
+              target="_blank"
+              rel="nooponer noreferrer"
+            >
+              Photographer
+            </a>{" "}
+            from NYC.
+          </h2>
+        </div>
+
+        <img src="/nickphoto.png" alt="Nick :)" id="photo" />
       </div>
-      <img src="/nickphoto.png" alt="Nick :)" id="photo" />
     </div>
   );
 }
