b9130c4d0cfd0238e9b13d9c65c3accbb5b91965
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-02-28 14:59:54 -0500

    Css changes to about section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,7 +8,9 @@ function Home() {
         <h1 className="info">CODE</h1>
         <h1 className="info">& PHOTO</h1>
         <h4 className="title">Full-Stack Software Developer</h4>
-        <h4 className="title" id="photographer">Photographer</h4>
+        <h4 className="title" id="photographer">
+          Photographer
+        </h4>
       </div>
       <img src="/nickphoto.png" alt="Nick :)" id="photo" />
     </div>
