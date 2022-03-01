b714245e84626cc727f1dca1547521a3f3c1bf62
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-02-28 21:53:20 -0500

    Added skills image w icons and changed css

--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -1,10 +1,12 @@
 import React from "react";
+import skills from "../../skills.png";
 import "./Skills.css";
 
 function Skills() {
   return (
     <div className="skills">
       <h2>SKILLS</h2>
+      <img src={skills} alt="coding languages" />
     </div>
   );
 }
