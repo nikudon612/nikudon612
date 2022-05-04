47b23c25bf65fe5155ca53f0063936aa0ae2d03f
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-05-04 19:24:39 -0400

    changed name of headshot image file

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -33,7 +33,7 @@ function Home() {
           </h4> */}
         </div>
 
-        <img src="Nick_HeadShot.png" alt="Nick :)" id="photo" />
+        <img src="Nick.png" alt="Nick :)" id="photo" />
       </div>
     </div>
   );
